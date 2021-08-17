61214b6b730d17f4002fb67258ef7efddadd0e14
A01638469 <A01638469@itesm.mx>
2021-08-17 20:28:22 +0000

    Act Clase 4 practica

--- assignments/01CantidadLitrosPintura/src/exercise.py
+++ assignments/01CantidadLitrosPintura/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    import math
+
+    area = float(input('Area a pintar en metros: '))
+    rendimiento = float(input('Rendimiento (m2/l): '))
+    l_comprar = math.ceil(area / rendimiento)
+    print('Litros a comprar: ' + str(l_comprar))
 
 if __name__ == '__main__':
     main()
